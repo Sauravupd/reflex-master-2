e810b15714675b29169983c27e7c537bb86f84e4
Sauravupd <upd6196@gmail.com>
2026-03-06 11:20:00 +0545

    chore: update targetSdk to API level 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.sauravupd.twitchbubbles"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.sauravupd.twitchbubbles"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
     }
